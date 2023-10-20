f0cc00c535506970c8f6047e02a135d124007395
Marsaud olivier <cougies79700@gmail.com>
2023-10-20 21:31:14 +0200

    add form contact

---
+++ skeleton/src/Form/ContactType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('phone')
+            ->add('mail')
+            ->add('request')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
