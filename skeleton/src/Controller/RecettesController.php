e196d55518cdd61bf834cc3f8aba3fe5498bd69a
Marsaud olivier <cougies79700@gmail.com>
2023-10-28 22:09:22 +0200

    Add card to recipes

--- skeleton/src/Controller/RecettesController.php
+++ skeleton/src/Controller/RecettesController.php
@@ -6,12 +6,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Recipes;
-use Doctrine\ORM\EntityManagerInterface; // Importez la classe EntityManager
+use Doctrine\ORM\EntityManagerInterface;
 
 class RecettesController extends AbstractController
 {
     #[Route('/recettes', name: 'app_recettes')]
-    public function index(EntityManagerInterface $entityManager): Response // Injectez EntityManager
+    public function index(EntityManagerInterface $entityManager): Response 
     {
         $recipes = $entityManager->getRepository(Recipes::class)->findAll();
         return $this->render('recettes/index.html.twig', [
