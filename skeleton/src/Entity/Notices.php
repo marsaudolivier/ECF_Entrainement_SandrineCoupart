0ae9c34a23f2eeb90d2a42e6ad448762c235adb8
Marsaud olivier <cougies79700@gmail.com>
2023-11-13 11:14:09 +0100

    update entity notices add comment

--- skeleton/src/Entity/Notices.php
+++ skeleton/src/Entity/Notices.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\NoticesRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: NoticesRepository::class)]
@@ -18,6 +19,12 @@ class Notices
 
     #[ORM\ManyToOne(inversedBy: 'notices')]
     private ?Recipes $recipes = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $comment = null;
+
+    #[ORM\Column]
+    private ?int $user = null;
 
     public function getId(): ?int
     {
@@ -44,6 +51,30 @@ class Notices
     public function setRecipes(?Recipes $recipes): static
     {
         $this->recipes = $recipes;
+
+        return $this;
+    }
+
+    public function getComment(): ?string
+    {
+        return $this->comment;
+    }
+
+    public function setComment(string $comment): static
+    {
+        $this->comment = $comment;
+
+        return $this;
+    }
+
+    public function getUser(): ?int
+    {
+        return $this->user;
+    }
+
+    public function setUser(int $user): static
+    {
+        $this->user = $user;
 
         return $this;
     }
